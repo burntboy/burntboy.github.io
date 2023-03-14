f4d825c0594025db1c4713d54896337e5f14eca6
burntboy <jjklambake@gmail.com>
2023-03-14 15:10:06 -0700

    Updates

--- _app/immutable/chunks/1.678ece4f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.48029b2c.js";export{t as component};
---
+++ _app/immutable/chunks/2.a087e558.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.8ad1c950.js";export{t as component};
